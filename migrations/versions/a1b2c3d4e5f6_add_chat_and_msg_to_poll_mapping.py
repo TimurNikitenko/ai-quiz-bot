60dc392ffc2a1e1de0c0550ca5cd2a06fb00dde6
TimurNikitenko <st128375@student.spbu.ru>
2026-07-09 15:24:12 +0100

    fix: linearize alembic migrations history

--- migrations/versions/a1b2c3d4e5f6_add_chat_and_msg_to_poll_mapping.py
+++ migrations/versions/a1b2c3d4e5f6_add_chat_and_msg_to_poll_mapping.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, Sequence[str], None] = '7a3ea1cb26bb'
+down_revision: Union[str, Sequence[str], None] = 'fcc1d8034480'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
