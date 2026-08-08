8923c78044a8996591775d1b96c4df15d7b24421
TimurNikitenko <st128375@student.spbu.ru>
2026-08-08 09:29:12 +0100

    refactor(services): extract PostLLMProcessorService for post analysis

--- services/__init__.py
+++ services/__init__.py
@@ -1,7 +1,9 @@
 """Пакет бизнес-сервисов приложения."""
 
 from .ingestion import PostIngestionService
+from .llm_processor import PostLLMProcessorService
 
 __all__ = [
     "PostIngestionService",
+    "PostLLMProcessorService",
 ]
